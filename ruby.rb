c0e61dfef0cf3bb2e1ce3066e6b2f37b6b490798
Shawn Conboy <conboysbusiness@gmail.com>
2023-12-30 01:43:59 -0500

    test

---
+++ ruby.rb
@@ -0,0 +1,39 @@
+# i = 0
+
+# loop do
+#     puts "i is #{i}"
+#     i += 1
+#     break if i == 10
+# end
+
+# i = 0
+# while i < 10 do 
+#     puts "i is #{i}"
+#     i += 1
+# end
+
+# while gets.chomp != 'yes' do
+#     puts "Are we there yet?"
+# end
+
+# i = 0
+# until i >= 10 do
+#  puts "i is #{i}"
+#  i += 1
+# end
+
+# ranges
+
+# (1..5)
+# (1...5)
+
+# ('a'..'d')
+
+for i in 0..5
+    puts "#{i} zombies incoming"
+end
+
+
+5.times do
+    puts 'hello world'
+end
