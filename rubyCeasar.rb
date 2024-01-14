8d6317b4fdb650526b7a592653198e84d2875127
Shawn Conboy <conboysbusiness@gmail.com>
2024-01-13 19:21:25 -0500

    updated code for better readability

--- rubyCeasar.rb
+++ rubyCeasar.rb
@@ -1,36 +1,35 @@
 def caesar_cipher(text, shift)
-    encrypted_text = ""
-    original_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    encrypted_letters = "BCDEFGHIJKLMNOPQRSTUVWXYZAbcdefghijklmnopqrstuvwxyza"
-  
-    text.each_char do |char|
-      if original_letters.include?(char)
-        char_index = original_letters.index(char)
-        encrypted_char = encrypted_letters[char_index]
-        encrypted_text += encrypted_char
-      else
-        encrypted_text += char
-      end
+  encrypted_text = ""
+
+  text.each_char do |char|
+    if char.match?(/[a-zA-Z]/)
+      offset = char.match?(/[a-z]/) ? 'a'.ord : 'A'.ord
+      encrypted_char = ((char.ord - offset + shift) % 26 + offset).chr
+      encrypted_text += encrypted_char
+    else
+      encrypted_text += char
     end
-  
-    encrypted_text
   end
-  
-  def main
-    puts "Welcome to the Caesar Cipher Encryption Program!"
-  
-    print "Enter text to encrypt: "
-    text = gets.chomp
-  
-    print "Enter the number of spaces to shift the encryption key: "
-    shift = gets.chomp.to_i
-  
-    encrypted_text = caesar_cipher(text, shift)
-  
-    puts "Encrypted Text: #{encrypted_text}"
-  end
-  
-  if __FILE__ == $PROGRAM_NAME
-    main
-  end
-  
+
+  encrypted_text
+end
+
+def main
+  puts
+  puts
+  puts "Welcome to the Caesar Cipher Encryption Program!"
+
+  print "Enter text to encrypt: "
+  text = gets.chomp
+
+  print "Enter the number of spaces to shift the encryption key: "
+  shift = gets.chomp.to_i
+
+  encrypted_text = caesar_cipher(text, shift)
+
+  puts "Encrypted Text: #{encrypted_text}"
+end
+
+# if __FILE__ == $PROGRAM_NAME
+  main
+# end
